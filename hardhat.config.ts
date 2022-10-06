05ede3e53459e5e079ccf7967949b84445d8dbe5
Sumit Ghosh <sumit@sumit-ghosh.com>
2022-10-07 00:49:36 +0530

    Add typechain publishing info

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -3,6 +3,9 @@ import "@nomicfoundation/hardhat-toolbox";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.17",
+  typechain: {
+    outDir: "typechain-types/src",
+  },
 };
 
 export default config;
